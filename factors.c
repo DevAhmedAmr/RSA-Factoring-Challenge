ee2c09e92964f7f665c65f3adbcd3ccaaaf35cc5
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-01 06:54:49 +0300

    first attempt in 0. Factorize all the things! task (A.C)

--- factors.c
+++ factors.c
@@ -1,34 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+int factorize(char *buffer);
+
 unsigned long int _atoi(char *line);
 int main(int argc, char **argv)
 {
 	char *line = NULL;
 
 	(void)argc;
-	size_t size = 0, i = 2;
+	size_t size = 0;
 	FILE *file = fopen(argv[1], "r");
 
 	while (getline(&line, &size, file) != -1)
 	{
-		unsigned long int num;
+		u_int32_t num, i = 2;
 
 		if (line[strlen(line) - 1] == '\n')
 			line[strlen(line) - 1] = '\0';
 
 		num = _atoi(line);
 
-		while (i < num)
-		{
-			if (num % i == 0)
-			{
-				unsigned long int x = num / i;
-				printf("%lu=%lu*%lu\n", num, x, i);
-				break;
-			}
-			i++;
-		}
+		factorize(line);
 	};
 
 	free(line);
@@ -57,4 +51,23 @@ unsigned long int _atoi(char *line)
 		sum = sum * -1;
 	}
 	return sum;
+}
+int factorize(char *buffer)
+{
+
+	u_int32_t num;
+	u_int32_t i;
+
+	num = atoi(buffer);
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+		{
+			printf("%d=%d*%d\n", num, num / i, i);
+			break;
+		}
+	}
+
+	return (0);
 }
