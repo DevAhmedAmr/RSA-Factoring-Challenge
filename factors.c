56d42fcace42e4b5bd7f8e188b9373b7d414e441
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-01 06:04:40 +0300

    first attempt in 0. Factorize all the things! task (A.C)

---
+++ factors.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+unsigned long int _atoi(char *line);
+int main(int argc, char **argv)
+{
+	char *line = NULL;
+
+	(void)argc;
+	size_t size = 0, i = 2;
+	FILE *file = fopen(argv[1], "r");
+
+	while (getline(&line, &size, file) != -1)
+	{
+		unsigned long int num;
+
+		if (line[strlen(line) - 1] == '\n')
+			line[strlen(line) - 1] = '\0';
+
+		num = _atoi(line);
+
+		while (1)
+		{
+			if (num % i == 0)
+			{
+				unsigned long int x = num / i;
+				printf("%lu=%lu*%lu\n", num, x, i);
+				break;
+			}
+			i++;
+		}
+	};
+}
+unsigned long int _atoi(char *line)
+{
+	unsigned long int sum = 0, i = 0;
+	int is_negative = 0;
+
+	if (line[0] == '-')
+	{
+		is_negative = 1;
+		i = 1;
+	}
+
+	for (; i < strlen(line); i++)
+	{
+		sum = sum * 10;
+		sum += line[i] - 48;
+	}
+
+	if (is_negative)
+	{
+		sum = sum * -1;
+	}
+	return sum;
+}
