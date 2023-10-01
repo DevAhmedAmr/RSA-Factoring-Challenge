108fdcbac40c9b86685a4c064836231b578eaf08
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-01 08:22:18 +0300

    first commit (A.C)

---
+++ factor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+
+text_file = open(sys.argv[1],'r')
+def factorize(line):
+    num = int(line)
+    i = 2
+    while(i < num):
+        
+        if num % i == 0:
+            print(f"{num:d}={int(num/i):d}*{i:d}",)
+            break
+        i=i+1
+
+
+while(1):
+    
+        line = text_file.readline()
+        
+        if  line == "":
+            break
+        
+        factorize(line)
+        
+        
+
+	
